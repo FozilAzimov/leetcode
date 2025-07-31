42f1eba1de1974378a9c1ea51a3f4ab4956c0dd4
FozilAzimov <foziljonweb@gmail.com>
2025-07-31 22:49:05 +0500

    Filter Elements from Array exercise added

--- easy/filter_elements_from_array.js
+++ easy/filter_elements_from_array.js
@@ -2,18 +2,18 @@
 // 2634. Filter Elements from Array
 
 // Answare:
-// let filter = (arr, fn) => {
-//   let newArr = [];
-//   for (let i = 0; i < arr.length; i++) if (fn(arr[i], i)) newArr.push(arr[i]);
-//   return newArr;
-// };
-// console.log(
-//   filter([0, 10, 20, 30], function greaterThan10(n) {
-//     return n > 10;
-//   })
-// );
-// console.log(
-//   filter([1, 2, 3], function firstIndex(n, i) {
-//     return i === 0;
-//   })
-// );
+let filter = (arr, fn) => {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) if (fn(arr[i], i)) newArr.push(arr[i]);
+  return newArr;
+};
+console.log(
+  filter([0, 10, 20, 30], function greaterThan10(n) {
+    return n > 10;
+  })
+);
+console.log(
+  filter([1, 2, 3], function firstIndex(n, i) {
+    return i === 0;
+  })
+);
