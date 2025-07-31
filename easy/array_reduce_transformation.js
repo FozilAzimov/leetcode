bcd16c8df7f1d0e36262a4625e0f844b4ddfb8d3
FozilAzimov <foziljonweb@gmail.com>
2025-07-31 22:46:48 +0500

    Array Reduce Transformation exercise added

--- easy/array_reduce_transformation.js
+++ easy/array_reduce_transformation.js
@@ -1,13 +1,13 @@
 // 2626. Array Reduce Transformation
 
 // Answare:
-// let reduce = (nums, fn, init) => nums.reduce(fn, init);
-// console.log(
-//   reduce(
-//     [1, 2, 3, 4],
-//     function sum(accum, curr) {
-//       return accum + curr * curr;
-//     },
-//     100
-//   )
-// );
+let reduce = (nums, fn, init) => nums.reduce(fn, init);
+console.log(
+  reduce(
+    [1, 2, 3, 4],
+    function sum(accum, curr) {
+      return accum + curr * curr;
+    },
+    100
+  )
+);
