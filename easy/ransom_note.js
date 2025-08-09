1c9c50845f43c04ac690ef1c8c70bc7e6bd02a79
FozilAzimov <foziljonweb@gmail.com>
2025-08-09 22:25:59 +0500

    letters from magazine and false otherwise.

---
+++ easy/ransom_note.js
@@ -0,0 +1,35 @@
+// Question:
+// 383. Ransom Note
+
+// letters from magazine and false otherwise.
+
+// Each letter in magazine can only be used once in ransomNote.
+
+// Example 1:
+
+// Input: ransomNote = "a", magazine = "b"
+// Output: false
+// Example 2:
+
+// Input: ransomNote = "aa", magazine = "ab"
+// Output: false
+// Example 3:
+
+// Input: ransomNote = "aa", magazine = "aab"
+// Output: true
+
+// Answare
+let canConstruct = (ransomNote, magazine) => {
+  let count = 0;
+  let len = ransomNote.length;
+  for (let value of ransomNote) {
+    if (magazine.includes(value)) {
+      count++;
+      ransomNote = ransomNote.replace(value, "");
+      magazine = magazine.replace(value, "");
+    }
+  }
+  return count == len;
+};
+
+console.log(canConstruct("aa", "ab"));
