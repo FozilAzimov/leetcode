f4d9789ef1d4255f36d460d04eccc920672ecc65
FozilAzimov <foziljonweb@gmail.com>
2025-08-13 13:49:22 +0500

    Pow(x, n) exercise added

---
+++ medium/pow.js
@@ -0,0 +1,18 @@
+let myPow = (x, n) => {
+  if (!n) return 1;
+  if (n < 0) {
+    x = 1 / x;
+    n = -n;
+  }
+  let res = 1;
+  while (n > 0) {
+    if (n % 2) res *= x;
+    x *= x;
+    n = Math.floor(n / 2);
+  }
+  return res;
+};
+
+console.log(myPow(2.0, 10));
+console.log(myPow(2.1, 3));
+console.log(myPow(2.0, -2));
